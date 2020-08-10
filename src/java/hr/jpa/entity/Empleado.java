24165b8a8f45ff482036905f44fe4b903057c000
MarioGonzalez99 <39312530+MarioGonzalez99@users.noreply.github.com>
2020-08-09 19:08:03 -0600

    Modificacion de constructor de la entidad de empleado

--- src/java/hr/jpa/entity/Empleado.java
+++ src/java/hr/jpa/entity/Empleado.java
@@ -12,6 +12,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -44,7 +46,8 @@ import javax.persistence.TemporalType;
 public class Empleado implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
+    @Id 
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Basic(optional = false)
     @Column(name = "EMP_ID")
     private Integer empId;
